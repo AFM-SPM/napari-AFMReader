beb4bb2af65fedb16940f0df1cd8873f99e1ce10
ahobbs7 <ahobbs834@gmail.com>
2025-11-13 15:33:31 +0000

    Removing redundant line

--- napari-afmreader/src/napari_afmreader_tests/_tests/test_reader.py
+++ napari-afmreader/src/napari_afmreader_tests/_tests/test_reader.py
@@ -76,10 +76,9 @@ RESOURCES = BASE_DIR / "napari-afmreader" / "src" / "napari_afmreader_tests" / "
             {},
             id="load topostats image valid pass.",
         ),
-        # NEW TEST: channel kwarg provided → no dialog expected
         pytest.param(
             str(RESOURCES / "file.spm"),
-            [("Height", True)],  # gets ignored because dialog never called
+            [("Height", True)], 
             ["Extracted channel Height"],
             {"channel": "Height"},
             id="load spm valid pass with channel kwarg.",
@@ -100,12 +99,8 @@ def test_get_reader_returns_callable(
         # Capture the message shown in the dialogue
         _, message = args[1], args[2]
         messages_seen.append(message)
-
-        # First call returns a wrong channel
-        if len(messages_seen) == 1:
-            return side_effect[0]
         # Second call returns the test's desired input
-        return side_effect[1]
+        return side_effect[0]
 
     # simulate QtPy dialogue box as this causes pytest to crash - need to add patch to where it is called
     with patch(
